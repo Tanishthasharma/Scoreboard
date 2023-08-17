c326aa906491db7c1c197a8acd54405accf1efdf
Tanishthasharma <sharmatanishtha1@gmail.com>
2023-08-17 11:09:03 +0530

    app

--- src/component/display/index.js
+++ src/component/display/index.js
@@ -2,47 +2,49 @@ import React from 'react'
 import { useState } from 'react';
 // import { Adder}   from './component/adder'
 import {Wrapper} from './style'
+import Mark from '../mark';
 
 export const Display = () => {
-    const[score, setScore ] = useState(0);
+    
     const[wicket, setWickets ] = useState(0);
     // const[overs,setOvers] = useState(0);
     // const [past, setPast] = useState([]);
     // const [present, setPresent] = useState(null);
     const[balls, setBalls] = useState(0);
+    const[score, setScore ] = useState(0);
 
-    const add0 = () =>{
-      setScore(score + 0)
-      setBalls(balls + 1)
-     }
-     const add1 = () =>{
-      setScore(score + 1)
-      setBalls(balls + 1)
-     }
-     const add2 = () =>{
-      setScore(score + 2)
-      setBalls(balls + 1)
-     }
-     const add3 = () =>{
-      setScore(score + 3)
-      setBalls(balls + 1)
-     }
-     const add4 = () =>{
-      setScore(score + 4)
-      setBalls(balls + 1)
-     }
-     const add6 = () =>{
-      setScore(score + 6)
-      setBalls(balls + 1)
-     }
+    // const add0 = () =>{
+    //   setScore(score + 0)
+    //   setBalls(balls + 1)
+    //  }
+    //  const add1 = () =>{
+    //   setScore(score + 1)
+    //   setBalls(balls + 1)
+    //  }
+    //  const add2 = () =>{
+    //   setScore(score + 2)
+    //   setBalls(balls + 1)
+    //  }
+    //  const add3 = () =>{
+    //   setScore(score + 3)
+    //   setBalls(balls + 1)
+    //  }
+    //  const add4 = () =>{
+    //   setScore(score + 4)
+    //   setBalls(balls + 1)
+    //  }
+    //  const add6 = () =>{
+    //   setScore(score + 6)
+    //   setBalls(balls + 1)
+    //  }
 
-     const out = () =>{
-        setWickets(wicket + 1)
-        }
+    //  const out = () =>{
+    //     setWickets(wicket + 1)
+    //     }
 
-        const wide = () =>{
-          setBalls(balls - 1);
-        }
+    //     const wide = () =>{
+    //       setBalls(balls - 1);
+    //     }
       
     //  const undo = () =>{
     //   if(past.length == 0) return;
@@ -53,8 +55,7 @@ export const Display = () => {
     //   setPast(newPast);
     //   setPresent(newPresent);
     //  }
-
-
+ 
   return (
     <>
       {
@@ -65,16 +66,16 @@ export const Display = () => {
       <div className='run'>
         {
         <div>
-            <input type='button' value="0" onClick={add0} />
-              <input type='button' value="1" onClick={add1} />
-               <input type='button' value="2" onClick={add2} />
+            {/* <input type='button' value="0" onClick={add0} /> */}
+              <input type='button'  value='1' onClick={() => <Mark  run={1}/>} />
+               {/* <input type='button' value="2" onClick={add2} />
                <input type='button' value="3" onClick={add3} />
               <input type='button' value="4" onClick={add4} />
             <input type='button' value="6" onClick={add6} />
             <input type='button' value="Wide" onClick={wide} />
               <input type='button' value="No Ball"  />
             <input type='button' value="Out"  onClick={out} />
-            <input type='button' value="Undo"  />
+            <input type='button' value="Undo"  /> */}
       </div>
         }
       </div> 
--- src/component/display/style.js
+++ src/component/display/style.js
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 import { styled } from 'styled-components'
 
 export const Wrapper = styled.section`
@@ -7,14 +7,17 @@ background-image: linear-gradient(to bottom right,blanchedalmond ,chocolate);
 // background-image: linear-gradient(to bottom right, rgba(200,200,200,0), rgba(0,255,200,5));
 text-align:center;
 padding:10px 10px;
+align-item:center;
+ justify-content:space-evenly;
 
 .run{
     background-color:rosybrown;
     height:250px;
-    width:400px;
+    width:60%;
     padding:70px 20px;
-    margin-left:530px;
-    margin-top:100px;
+    margin:50px auto;
+    // margin-left:auto;
+    // margin-top:100px;
     border-radius: 50px;
     box-shadow: 5px 5px gray;
 }
@@ -23,10 +26,11 @@ padding:10px 10px;
     border: 2px ;
     background-color:lemonchiffon;
     color:peru;
-    width:120px;
-    margin-right:5px;
-    margin-top:10px;
-    padding:15px 10px;
+    width:30%;
+    //margin:10px auto;
+    //  margin-right:5px;
+      margin-top:10px;
+    padding:15px 0px;
     justify-content:space-between;
     border-radius: 20px;
     box-shadow: 3px 3px gray;
