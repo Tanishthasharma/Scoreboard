3a44209f6af81b2acefa82affe2216e168083183
Tanishthasharma <sharmatanishtha1@gmail.com>
2023-08-17 08:46:03 +0530

    display

--- src/component/display/index.js
+++ src/component/display/index.js
@@ -7,30 +7,41 @@ export const Display = () => {
     const[score, setScore ] = useState(0);
     const[wicket, setWickets ] = useState(0);
     // const[overs,setOvers] = useState(0);
-    const [past, setPast] = useState([]);
-    const [present, setPresent] = useState(null);
+    // const [past, setPast] = useState([]);
+    // const [present, setPresent] = useState(null);
+    const[balls, setBalls] = useState(0);
 
     const add0 = () =>{
       setScore(score + 0)
+      setBalls(balls + 1)
      }
      const add1 = () =>{
       setScore(score + 1)
+      setBalls(balls + 1)
      }
      const add2 = () =>{
       setScore(score + 2)
+      setBalls(balls + 1)
      }
      const add3 = () =>{
       setScore(score + 3)
+      setBalls(balls + 1)
      }
      const add4 = () =>{
       setScore(score + 4)
+      setBalls(balls + 1)
      }
      const add6 = () =>{
       setScore(score + 6)
+      setBalls(balls + 1)
      }
 
      const out = () =>{
         setWickets(wicket + 1)
+        }
+
+        const wide = () =>{
+          setBalls(balls - 1);
         }
       
     //  const undo = () =>{
@@ -50,6 +61,7 @@ export const Display = () => {
     <Wrapper>
         <h1>{score}/{wicket}</h1>
       <h2>9.8 overs</h2>
+      <p>Balls:{balls}</p>
       <div className='run'>
         {
         <div>
@@ -59,7 +71,7 @@ export const Display = () => {
                <input type='button' value="3" onClick={add3} />
               <input type='button' value="4" onClick={add4} />
             <input type='button' value="6" onClick={add6} />
-            <input type='button' value="Wide"  />
+            <input type='button' value="Wide" onClick={wide} />
               <input type='button' value="No Ball"  />
             <input type='button' value="Out"  onClick={out} />
             <input type='button' value="Undo"  />
