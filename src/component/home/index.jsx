9426723b18758c56dea8ca9caa24d826c73de331
Rahul Jain <jainraahul03@gmail.com>
2023-08-21 09:42:35 +0530

    home updated

--- src/component/home/index.jsx
+++ src/component/home/index.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    <div>
+    <main>
+      <div>
       <h1>Welcome to our website .</h1>
     </div>
+    <footer>
+      &copy; 2023, All rights reserved .
+    </footer>
+    
+    </main>
+    
   )
 }
 
